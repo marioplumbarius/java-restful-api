0df01721faeaf26f15b5faec7d00fef0b481cab5
mario.souza <mario.souza@walmart.com>
2015-01-14 19:12:05 -0200

    added POST /api/users route to project

--- hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/resource/UserResource.java
+++ hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/resource/UserResource.java
@@ -3,6 +3,7 @@ package com.mario.java.restful.api.hibernate.jpa.resource;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -11,10 +12,10 @@ import com.mario.java.restful.api.hibernate.jpa.domain.User;
 import com.mario.java.restful.api.hibernate.jpa.service.UserService;
 
 @Path("/users")
+@Produces("application/json")
 public class UserResource {
 
     @GET
-    @Produces("application/json")
     public List<User> findAll() {
         UserService userService = new UserService();
         List<User> users = userService.findAll();
@@ -24,11 +25,19 @@ public class UserResource {
 
     @GET
     @Path("{id}")
-    @Produces("application/json")
     public User findById(@PathParam("id") Long id) {
         UserService userService = new UserService();
         User user = userService.findById(id);
 
         return user;
     }
+
+    @POST
+    public String create(User user) {
+        UserService userService = new UserService();
+        userService.persist(user);
+
+        return null;
+    }
+
 }
