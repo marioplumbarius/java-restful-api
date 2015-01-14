bd773f588b6e2921762813a88739f1d5882f4897
mario.souza <mario.souza@walmart.com>
2015-01-14 16:21:47 -0200

    moved getByName resource entry to findAll

--- hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/resource/UserResource.java
+++ hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/resource/UserResource.java
@@ -6,43 +6,29 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-
-import org.hibernate.Query;
-import org.hibernate.Session;
 
 import com.mario.java.restful.api.hibernate.jpa.domain.User;
 import com.mario.java.restful.api.hibernate.jpa.service.UserService;
-import com.mario.java.restful.api.hibernate.jpa.util.SessionManager;
 
 @Path("/users")
 public class UserResource {
 
-	@GET
-	@Produces("application/json")
-	public User getByName(@QueryParam("name") String name) {
-		Session session = SessionManager.getSessionFactory().openSession();
+    @GET
+    @Produces("application/json")
+    public List<User> findAll() {
+        UserService userService = new UserService();
+        List<User> users = userService.findAll();
 
-		session.beginTransaction();
+        return users;
+    }
 
-		User user = new User(name);
+    @GET
+    @Path("{id}")
+    @Produces("application/json")
+    public User findById(@PathParam("id") Long id) {
+        UserService userService = new UserService();
+        User user = userService.findById(id);
 
-		session.save(user);
-		session.getTransaction().commit();
-
-		Query q = session.createQuery("From User ");
-		List<User> resultList = q.list();
-
-		return resultList.get(0);
-	}
-
-	@GET
-	@Path("{id}")
-	@Produces("application/json")
-	public User getById(@PathParam("id") Long id) {
-		UserService userService = new UserService();
-		User user = userService.findById(id);
-
-		return user;
-	}
+        return user;
+    }
 }
