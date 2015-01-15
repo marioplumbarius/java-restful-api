1c78b62ab67b4d432f8f9fec46877004c3930413
mario.souza <mario.souza@walmart.com>
2015-01-14 22:13:44 -0200

    handling dao level exceptions and providing a friendly message in the request response body

--- hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/resource/UserResource.java
+++ hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/resource/UserResource.java
@@ -1,14 +1,18 @@
 package com.mario.java.restful.api.hibernate.jpa.resource;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import com.mario.java.restful.api.hibernate.jpa.domain.User;
+import com.mario.java.restful.api.hibernate.jpa.resource.exception.ValidationExceptionHandler;
 import com.mario.java.restful.api.hibernate.jpa.service.UserService;
 
 @Path("/users")
@@ -25,7 +29,7 @@ public class UserResource {
 
     @GET
     @Path("{id}")
-    public User findById(@PathParam("id") Long id) {
+    public User find(@PathParam("id") Long id) {
         UserService userService = new UserService();
         User user = userService.findById(id);
 
@@ -33,11 +37,22 @@ public class UserResource {
     }
 
     @POST
-    public String create(User user) {
+    public Response create(User user) {
+        Response res = null;
         UserService userService = new UserService();
-        userService.persist(user);
 
-        return null;
+        try {
+            userService.persist(user);
+            res = Response.created(null).build();
+        } catch (Exception e) {
+            ValidationExceptionHandler validator = new ValidationExceptionHandler();
+            Map<String, String> errors = null;
+
+            if (!validator.isValid(user)) errors = validator.getErrors();
+
+            res = Response.status(Status.BAD_REQUEST).entity(errors).build();
+        }
+
+        return res;
     }
-
 }
