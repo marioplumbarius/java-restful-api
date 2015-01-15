252ebba4a72fd1bc8527ff5967fceb7baa16be63
mario.souza <mario.souza@walmart.com>
2015-01-14 22:12:16 -0200

    replacing session manager for new class

--- hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/dao/implementation/UserDaoImplementation.java
+++ hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/dao/implementation/UserDaoImplementation.java
@@ -4,44 +4,44 @@ import java.util.List;
 
 import com.mario.java.restful.api.hibernate.jpa.dao.interfacee.UserDaoInterface;
 import com.mario.java.restful.api.hibernate.jpa.domain.User;
-import com.mario.java.restful.api.hibernate.jpa.util.SessionManagerDao;
+import com.mario.java.restful.api.hibernate.jpa.util.SessionManager;
 
 public class UserDaoImplementation implements UserDaoInterface<User, Long> {
-	public SessionManagerDao sessionManager = new SessionManagerDao();
+    public SessionManager sessionManager = new SessionManager();
 
-	@Override
-	public void persist(User entity) {
-		this.sessionManager.getSession().save(entity);
-	}
+    @Override
+    public void persist(User entity) {
+        this.sessionManager.getSession().save(entity);
+    }
 
-	@Override
-	public void update(User entity) {
-		this.sessionManager.getSession().update(entity);
-	}
+    @Override
+    public void update(User entity) {
+        this.sessionManager.getSession().update(entity);
+    }
 
-	@Override
-	public User findById(Long id) {
-		User user = (User) this.sessionManager.getSession().get(User.class, id);
-		return user;
-	}
+    @Override
+    public User findById(Long id) {
+        User user = (User) this.sessionManager.getSession().get(User.class, id);
+        return user;
+    }
 
-	@Override
-	public void delete(User entity) {
-		this.sessionManager.getSession().delete(entity);
-	}
+    @Override
+    public void delete(User entity) {
+        this.sessionManager.getSession().delete(entity);
+    }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<User> findAll() {
-		List<User> users = this.sessionManager.getSession()
-				.createQuery("from User").list();
-		return users;
-	}
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<User> findAll() {
+        List<User> users = this.sessionManager.getSession()
+                .createQuery("from User").list();
+        return users;
+    }
 
-	@Override
-	public void deleteAll() {
-		List<User> users = this.findAll();
-		for (User user : users)
-			this.delete(user);
-	}
+    @Override
+    public void deleteAll() {
+        List<User> users = this.findAll();
+        for (User user : users)
+            this.delete(user);
+    }
 }
