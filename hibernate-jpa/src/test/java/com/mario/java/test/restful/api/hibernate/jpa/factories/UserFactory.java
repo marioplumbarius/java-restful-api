07cde8086a90b526284b1a3d0057864cd2ed835a
mario.souza <mario.souza@walmart.com>
2015-01-19 11:47:38 -0200

    created URI factory for User.class

--- hibernate-jpa/src/test/java/com/mario/java/test/restful/api/hibernate/jpa/factories/UserFactory.java
+++ hibernate-jpa/src/test/java/com/mario/java/test/restful/api/hibernate/jpa/factories/UserFactory.java
@@ -3,6 +3,9 @@ package com.mario.java.test.restful.api.hibernate.jpa.factories;
 import com.mario.java.restful.api.hibernate.jpa.domain.User;
 
 public final class UserFactory {
+
+    public static String URI = "/users/null";
+
     public static User createInvalidUser() {
         return new User("invalid name, so many characters, wowwwww");
     }
