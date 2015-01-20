8f0cfea53e7cadc2a03495177543cdabee084b90
mario.souza <mario.souza@walmart.com>
2015-01-20 10:25:05 -0200

    fixed hibernate session manager setup. It was being setup every time it was called

--- hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/util/SessionManager.java
+++ hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/util/SessionManager.java
@@ -7,56 +7,55 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 public class SessionManager {
-	private Session session;
-	private Transaction transaction;
+    private Session session;
+    private SessionFactory sessionFactory;
+    private Transaction transaction;
 
-	public SessionManager() {
-	}
+    public SessionManager() {
+        this.sessionFactory = this.getSessionFactory();
+    }
 
-	public Session openSession() {
-		this.session = getSessionFactory().openSession();
-		return this.session;
-	}
+    public Session openSession() {
+        this.session = this.sessionFactory.openSession();
+        return this.session;
+    }
 
-	public Session openSessionWithTransaction() {
-		this.session = getSessionFactory().openSession();
-		this.transaction = this.session.beginTransaction();
-		return this.session;
-	}
+    public Session openSessionWithTransaction() {
+        this.session = this.sessionFactory.openSession();
+        this.transaction = this.session.beginTransaction();
+        return this.session;
+    }
 
-	public void closeSession() {
-		this.session.close();
-	}
+    public void closeSession() {
+        this.session.close();
+    }
 
-	public void closeSessionWithTransaction() {
-		this.transaction.commit();
-		this.session.close();
-	}
+    public void closeSessionWithTransaction() {
+        this.transaction.commit();
+        this.session.close();
+    }
 
-	private static SessionFactory getSessionFactory() {
-		Configuration configuration = new Configuration().configure();
-		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
-				.applySettings(configuration.getProperties());
-		SessionFactory sessionFactory = configuration
-				.buildSessionFactory(builder.build());
+    public Session getSession() {
+        return this.session;
+    }
 
-		return sessionFactory;
-	}
+    public void setSession(Session session) {
+        this.session = session;
+    }
 
-	public Session getSession() {
-		return this.session;
-	}
+    public Transaction getTransaction() {
+        return this.transaction;
+    }
 
-	public void setSession(Session session) {
-		this.session = session;
-	}
+    public void setTransaction(Transaction transaction) {
+        this.transaction = transaction;
+    }
 
-	public Transaction getTransaction() {
-		return this.transaction;
-	}
+    private SessionFactory getSessionFactory() {
+        Configuration configuration = new Configuration().configure();
+        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+        SessionFactory sessionFactory = configuration.buildSessionFactory(builder.build());
 
-	public void setTransaction(Transaction transaction) {
-		this.transaction = transaction;
-	}
-
+        return sessionFactory;
+    }
 }
