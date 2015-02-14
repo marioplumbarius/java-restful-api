55dadc09d9f6d7caef39ebd7664bcc2af05eaf12
Mario Luan <mariosouzaluan@gmail.com>
2015-02-13 23:02:42 -0200

    created spec for UserService#persist and UserService#update

--- hibernate-jpa/src/test/java/com/mario/java/test/restful/api/hibernate/jpa/service/UserServiceTest.java
+++ hibernate-jpa/src/test/java/com/mario/java/test/restful/api/hibernate/jpa/service/UserServiceTest.java
@@ -1,55 +1,64 @@
-//package com.mario.java.test.restful.api.hibernate.jpa.service;
-//
-//import static com.mscharhag.oleaster.runner.StaticRunnerSupport.afterEach;
-//import static com.mscharhag.oleaster.runner.StaticRunnerSupport.beforeEach;
-//import static com.mscharhag.oleaster.runner.StaticRunnerSupport.describe;
-//import static com.mscharhag.oleaster.runner.StaticRunnerSupport.it;
-//
-//import org.junit.runner.RunWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//
-//import com.mario.java.restful.api.hibernate.jpa.dao.implementation.UserDaoImplementation;
-//import com.mario.java.restful.api.hibernate.jpa.domain.User;
-//import com.mario.java.restful.api.hibernate.jpa.service.UserService;
-//import com.mscharhag.oleaster.runner.OleasterRunner;
-//
-//@RunWith(OleasterRunner.class)
-//public class UserServiceTest {
-//
-//    @Mock
-//    private UserService service;
-//
-//    @InjectMocks
-//    private UserDaoImplementation userDao;
-//
-//    private User user;
-//    {
-//        beforeEach(() -> {
-//            MockitoAnnotations.initMocks(this);
-//
-//            this.service = new UserService();
-//        });
-//
-//        afterEach(() -> {
-//            this.user = null;
-//        });
-//
-//        describe("#persist", () -> {
-//            beforeEach(() -> {
-////                this.user = UserFactory.createValidUser();
-//                this.service.persist(this.user);
-//            });
-//
-//            it("opens a new session with transaction", () -> {
-////                verify(this.userDao.sessionManager).openSessionWithTransaction();
-//            });
-//
-//            it("closes the session", () -> {
-////                verify(this.userDao.sessionManager).closeSessionWithTransaction();
-//            });
-//
-//        });
-//    }
-//}
+package com.mario.java.test.restful.api.hibernate.jpa.service;
+
+import static com.mscharhag.oleaster.runner.StaticRunnerSupport.beforeEach;
+import static com.mscharhag.oleaster.runner.StaticRunnerSupport.describe;
+import static com.mscharhag.oleaster.runner.StaticRunnerSupport.it;
+
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import com.mario.java.restful.api.hibernate.jpa.domain.UserDomain;
+import com.mario.java.restful.api.hibernate.jpa.repository.CrudRepository;
+import com.mario.java.restful.api.hibernate.jpa.service.UserService;
+import com.mario.java.test.restful.api.hibernate.jpa.factories.IdFactory;
+import com.mscharhag.oleaster.runner.OleasterRunner;
+
+@RunWith(OleasterRunner.class)
+public class UserServiceTest {
+
+	@Mock
+	private UserDomain user;
+
+	@Mock
+	private CrudRepository<UserDomain, Long> userCrud;
+
+	private UserService userService;
+
+	private Long id;
+
+	{
+
+		beforeEach(()->{
+			MockitoAnnotations.initMocks(this);
+
+			this.userService = new UserService(this.userCrud);
+			this.id = IdFactory.createValidId();
+		});
+
+		describe("#persist", ()->{
+			beforeEach(()->{
+				this.userService.persist(this.user);
+			});
+
+			it("persists the users", ()->{
+				Mockito.verify(this.userCrud).persist(this.user);
+			});
+		});
+
+		describe("#update", ()->{
+			beforeEach(()->{
+				this.userService.update(this.id, this.user);
+			});
+
+			it("sets the id of the user", ()->{
+				Mockito.verify(this.user).setId(this.id);
+			});
+
+			it("updates the user", ()->{
+				Mockito.verify(this.userCrud).update(this.id, this.user);
+			});
+		});
+	}
+}
