dee90e3d59d202b08141f84cf3ec282f1aa7459e
Mario Luan <mariosouzaluan@gmail.com>
2015-02-18 14:31:57 -0200

    configuring when UserDomain's createdAt should be inserted/updated

--- hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/domain/DatedDomain.java
+++ hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/domain/DatedDomain.java
@@ -2,6 +2,7 @@ package com.mario.java.restful.api.hibernate.jpa.domain;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
 import org.hibernate.annotations.CreationTimestamp;
@@ -9,23 +10,27 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 @MappedSuperclass
 public abstract class DatedDomain {
-	
+
 	@CreationTimestamp
-    private Date createdAt;
+	@Column(insertable=true, updatable=false)
+	private Date createdAt;
 
-    @UpdateTimestamp
-    private Date updatedAt;
-    
-    public Date getCreatedAt() {
-        return this.createdAt;
-    }
+	@UpdateTimestamp
+	private Date updatedAt;
 
-    public Date getUpdatedAt() {
-        return this.updatedAt;
-    }
+	public Date getCreatedAt() {
+		return this.createdAt;
+	}
 
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-	
+	public Date getUpdatedAt() {
+		return this.updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
 }
