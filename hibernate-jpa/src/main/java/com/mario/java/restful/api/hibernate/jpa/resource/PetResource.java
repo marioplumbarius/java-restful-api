8812d9b82aed8da81ebcb6b0f5649725a274fff7
Mario Luan <mariosouzaluan@gmail.com>
2015-03-02 21:44:55 -0300

    returning 422 when pet.user is not found on create

--- hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/resource/PetResource.java
+++ hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/resource/PetResource.java
@@ -21,21 +21,25 @@ import org.hibernate.ObjectNotFoundException;
 
 import com.mario.java.restful.api.hibernate.jpa.annotations.PATCH;
 import com.mario.java.restful.api.hibernate.jpa.domain.PetDomain;
+import com.mario.java.restful.api.hibernate.jpa.domain.validation.DomainValidator;
 import com.mario.java.restful.api.hibernate.jpa.service.PetService;
+import com.mario.java.restful.api.hibernate.jpa.service.UserService;
 
 @Path("/pets")
 @Consumes("application/json")
 @Produces("application/json")
 public class PetResource {
 
-    private PetService service;
+    private PetService petService;
+    private UserService userService;
 
     public PetResource() {
-        this(new PetService());
+        this(new PetService(), new UserService());
     }
 
-    public PetResource(PetService service) {
-        this.service = service;
+    public PetResource(PetService petService, UserService userService) {
+        this.petService = petService;
+        this.userService = userService;
     }
 
     @GET
@@ -43,7 +47,7 @@ public class PetResource {
     public Response find(@PathParam("id") Long id) {
         Response res = null;
 
-        PetDomain pet = this.service.find(id);
+        PetDomain pet = this.petService.find(id);
 
         if (pet == null) {
             res = Response.status(Status.NOT_FOUND).build();
@@ -61,9 +65,9 @@ public class PetResource {
         if (name != null) {
             Map<String, String> criterias = new HashMap<String, String>();
             criterias.put("name", name);
-            pets = this.service.findAll(criterias);
+            pets = this.petService.findAll(criterias);
         } else {
-            pets = this.service.findAll();
+            pets = this.petService.findAll();
         }
 
         return pets;
@@ -74,9 +78,7 @@ public class PetResource {
         Response res = null;
 
         if (pet.isValid()) {
-            this.service.persist(pet);
-            URI uri = URI.create("/pets/" + pet.getId());
-            res = Response.created(uri).build();
+        	res = this.createHelper(pet);
         } else {
             res = Response.status(Status.BAD_REQUEST).entity(pet.getErrors()).build();
         }
@@ -90,12 +92,7 @@ public class PetResource {
         Response res = null;
 
         if(pet.isValid()){
-            try {
-                this.service.update(id, pet);
-                res = Response.noContent().build();
-            } catch (ObjectNotFoundException e) {
-                res = Response.status(Status.NOT_FOUND).build();
-            }
+            res = this.updateHelper(id, pet);
         } else {
             res = Response.status(Status.BAD_REQUEST).entity(pet.getErrors()).build();
         }
@@ -108,7 +105,7 @@ public class PetResource {
     public Response patch(@PathParam("id") Long id, PetDomain pet) {
         Response res = null;
 
-        PetDomain currentPet = this.service.find(id);
+        PetDomain currentPet = this.petService.find(id);
 
         if (currentPet != null) {
             pet.patch(currentPet);
@@ -126,12 +123,42 @@ public class PetResource {
         Response res = null;
 
         try {
-            this.service.delete(id);
+            this.petService.delete(id);
             res = Response.noContent().build();
         } catch (ObjectNotFoundException e) {
             res = Response.status(Status.NOT_FOUND).build();
         }
 
         return res;
+    }
+
+    private Response createHelper(PetDomain pet){
+    	Response res;
+
+    	if(this.userService.find(pet.getUserId()) != null){
+    		this.petService.persist(pet);
+            URI uri = URI.create("/pets/" + pet.getId());
+            res = Response.created(uri).build();
+    	} else {
+    		// TODO
+			// move error defined messages to a file
+			Map<String, String> errors = DomainValidator.buildError("userId", "not found");
+			res = Response.status(Status.NOT_FOUND).entity(errors).build();
+    	}
+
+    	return res;
+    }
+
+    private Response updateHelper(Long id, PetDomain pet){
+    	Response res;
+
+    	try {
+            this.petService.update(id, pet);
+            res = Response.noContent().build();
+        } catch (ObjectNotFoundException e) {
+            res = Response.status(Status.NOT_FOUND).build();
+        }
+
+    	return res;
     }
 }
