e594ab0188b4008317b4d58c6c28d46f9e1acba8
Mario Luan <mariosouzaluan@gmail.com>
2015-03-17 20:54:58 -0300

    implemented missing method from AbstractRepositoryEntityManagerImpl

--- hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/repository/impl/AbstractRepositoryEntityManagerImpl.java
+++ hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/repository/impl/AbstractRepositoryEntityManagerImpl.java
@@ -5,9 +5,10 @@ import java.util.List;
 import java.util.Map;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import com.mario.java.restful.api.hibernate.jpa.repository.AbstractRepository;
+import com.mario.java.restful.api.hibernate.jpa.repository.ObjectNofFoundException;
 import com.mario.java.restful.api.hibernate.jpa.repository.util.EntityManagerSingleton;
 
 public class AbstractRepositoryEntityManagerImpl<T, ID extends Serializable> implements AbstractRepository<T, ID> {
@@ -15,39 +16,30 @@ public class AbstractRepositoryEntityManagerImpl<T, ID extends Serializable> imp
 	private String entityName;
 	private Class<T> entityClass;
 	private EntityManager entityManager;
+	private String baseQuery;
 
 	public AbstractRepositoryEntityManagerImpl(String entityName, Class<T> entityClass) {
 		this(entityName, entityClass, EntityManagerSingleton.getInstance().getEntityManager());
 	}
 
-
 	public AbstractRepositoryEntityManagerImpl(String entityName, Class<T> entityClass, EntityManager entityManager) {
 		this.entityName = entityName;
 		this.entityClass = entityClass;
 		this.entityManager = entityManager;
+		this.baseQuery = "SELECT t FROM " + this.entityName + " t";
 	}
 
 	@Override
-	@SuppressWarnings("unchecked")
 	public List<T> findAll(){
-		Query query = this.entityManager.createQuery("SELECT e FROM " + this.entityName + " e");
-		List<T> entities = query.getResultList();
+		TypedQuery<T> typedQuery = this.entityManager.createQuery(this.baseQuery, this.entityClass);
+		List<T> entities = typedQuery.getResultList();
 
 		return entities;
 	}
 
 	@Override
-	public List<T> findAll(Map<String, Object> criterias) {
-
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public T find(ID id) {
-
-		// TODO Auto-generated method stub
-		return null;
+		return this.entityManager.find(this.entityClass, id);
 	}
 
 	@Override
@@ -60,7 +52,6 @@ public class AbstractRepositoryEntityManagerImpl<T, ID extends Serializable> imp
 			this.entityManager.getTransaction().rollback();
 			throw e;
 		}
-
 	}
 
 	@Override
@@ -77,27 +68,53 @@ public class AbstractRepositoryEntityManagerImpl<T, ID extends Serializable> imp
 
 	@Override
 	public void delete(T entity) throws Exception {
-
-		// TODO Auto-generated method stub
-
+		try {
+			this.entityManager.getTransaction().begin();
+			this.entityManager.remove(entity);
+			this.entityManager.getTransaction().commit();
+		} catch (Exception e) {
+			this.entityManager.getTransaction().rollback();
+			throw e;
+		}
 	}
 
 	@Override
 	public void deleteAll() throws Exception {
-
-		// TODO Auto-generated method stub
-
+		List<T> entities = this.findAll();
+		this.deleteAll(entities);
 	}
 
 	@Override
 	public void deleteAll(List<T> entities) throws Exception {
+		if(entities != null){
+			this.deleteAllHelper(entities);
+		} else {
+			throw new ObjectNofFoundException(this.entityName);
+		}
+	}
 
-		// TODO Auto-generated method stub
-
+	private void deleteAllHelper(List<T> entities) throws Exception {
+		try {
+			this.entityManager.getTransaction().begin();
+			for (T entity : entities) {
+				this.entityManager.remove(entity);
+			}
+			this.entityManager.getTransaction().commit();
+		} catch (Exception e) {
+			this.entityManager.getTransaction().rollback();
+			throw e;
+		}
 	}
 
 	@Override
-	public void deleteAll(Map<String, Object> criterias) throws Exception {
+	public List<T> findAll(Map<String, Object> restrictions) {
+
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteAll(Map<String, Object> restrictions) throws Exception {
 
 		// TODO Auto-generated method stub
 
