d9d3e148fd21c7d6dc5b14207e15fcfe0cf81e2f
Mario Luan <mariosouzaluan@gmail.com>
2015-03-17 20:46:38 -0300

    added documentation to AbstractRepository class

--- hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/repository/AbstractRepository.java
+++ hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/repository/AbstractRepository.java
@@ -8,23 +8,72 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * TODO
- * DOCUMENT ME!!!
+ * Interface for generic repository operations.
  * @author marioluan
  *
- * @param <T>
- * @param <ID>
+ * @param <T> the entity's type
+ * @param <ID> the entity's primary key type
  */
 public interface AbstractRepository<T, ID extends Serializable> {
 
+	/**
+	 * Finds all entities
+	 * @return all entities found or null
+	 */
 	public List<T> findAll();
-	public List<T> findAll(Map<String, Object> criterias);
-	public T find(ID id);
-	public void persist(T entity) throws Exception;
-	public void update(T entity) throws Exception;
-	public void delete(T entity) throws Exception;
-	public void deleteAll() throws Exception;
-	public void deleteAll(List<T> entities) throws Exception;
-	public void deleteAll(Map<String, Object> criterias) throws Exception;
 
+	/**
+	 * Finds all {@link T} entities matching the {@link Map<String, Object>} restrictions
+	 * @param restrictions the restrictions to be applied to the search
+	 * @return the entities found or null
+	 */
+	public List<T> findAll(Map<String, Object> restrictions);
+
+	/**
+	 * Finds an {@link T} entity by its {@link Serializable} id
+	 * @param id the id of the entity
+	 * @return the entity found or null
+	 */
+	public T find(ID id);
+
+	/**
+	 * Persists an {@link T} entity
+	 * @param entity the entity to be persisted
+	 * @throws Exception when the entity could not be persisted
+	 */
+	public void persist(T entity) throws Exception;
+
+	/**
+	 * Updates the {@link T} entity
+	 * @param entity the entity to override the existing one from database with the same id
+	 * @throws Exception when the entity could not be updated
+	 */
+	public void update(T entity) throws Exception;
+
+	/**
+	 * Deletes the {@link T} entity
+	 * @param entity the entity to be deleted
+	 * @throws Exception when the entity could not be deleted
+	 */
+	public void delete(T entity) throws Exception;
+
+	/**
+	 * Deletes all entities
+	 * @throws Exception when the entities could not be deleted
+	 */
+	public void deleteAll() throws Exception;
+
+	/**
+	 * Deletes all entities
+	 * @param entities the entities to be deleted
+	 * @throws Exception when the entities could not be deleted
+	 */
+	public void deleteAll(List<T> entities) throws Exception;
+
+	/**
+	 * Deletes all entities matching the restrictions
+	 * @param restrictions the restrictions to be applied to the search
+	 * @throws Exception when the entities could not be deleted
+	 */
+	public void deleteAll(Map<String, Object> restrictions) throws Exception;
 }
