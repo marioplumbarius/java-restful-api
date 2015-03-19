176e9b7576fa7b5b55604f46c8c98b725aed3537
Mario Luan <mariosouzaluan@gmail.com>
2015-03-19 03:54:09 -0300

    registering the UserResource with the new UserService implementation

--- hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/application/ApiApplication.java
+++ hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/application/ApiApplication.java
@@ -3,17 +3,25 @@ package com.mario.java.restful.api.hibernate.jpa.application;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.EntityManager;
 import javax.ws.rs.core.Application;
 
+import com.mario.java.restful.api.hibernate.jpa.domain.PetDomain;
+import com.mario.java.restful.api.hibernate.jpa.domain.UserDomain;
+import com.mario.java.restful.api.hibernate.jpa.repository.AbstractRepository;
+import com.mario.java.restful.api.hibernate.jpa.repository.impl.AbstractRepositoryJPAImpl;
+import com.mario.java.restful.api.hibernate.jpa.repository.util.EntityManagerSingleton;
 import com.mario.java.restful.api.hibernate.jpa.resource.PetResource;
 import com.mario.java.restful.api.hibernate.jpa.resource.UserResource;
+import com.mario.java.restful.api.hibernate.jpa.service.UserService;
+import com.mario.java.restful.api.hibernate.jpa.service.impl.UserServiceNewImpl;
 
 public class ApiApplication extends Application {
 	private Set<Object> singletons = new HashSet<Object>();
 	private Set<Class<?>> empty = new HashSet<Class<?>>();
 
 	public ApiApplication() {
-		this.singletons.add(new UserResource());
+		this.singletons.add(new UserResource(this.buildUserService()));
 		this.singletons.add(new PetResource());
 	}
 
@@ -27,4 +35,12 @@ public class ApiApplication extends Application {
 		return this.singletons;
 	}
 
+	// TODO - move this to a bean configuration and inject them
+	private UserService buildUserService(){
+		EntityManager entityManager = EntityManagerSingleton.getInstance().getEntityManager();
+		AbstractRepository<UserDomain, Long> abstractRepositoryUser = new AbstractRepositoryJPAImpl<UserDomain, Long>(UserDomain.class, entityManager);
+		AbstractRepository<PetDomain, Long> abstractRepositoryPet = new AbstractRepositoryJPAImpl<PetDomain, Long>(PetDomain.class, entityManager);
+
+		return new UserServiceNewImpl(abstractRepositoryUser, abstractRepositoryPet);
+	}
 }
