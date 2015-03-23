bd73777b33fd1ca56d4805d67f07f2625a309dce
Mario Luan <mariosouzaluan@gmail.com>
2015-03-23 02:57:30 -0300

    replaced concrete repositories classes for its interfaces + injecting it

--- hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/service/impl/UserServiceNewImpl.java
+++ hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/service/impl/UserServiceNewImpl.java
@@ -4,33 +4,41 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+
 import com.mario.java.restful.api.hibernate.jpa.domain.PetDomain;
-import com.mario.java.restful.api.hibernate.jpa.domain.PetDomain_;
 import com.mario.java.restful.api.hibernate.jpa.domain.UserDomain;
-import com.mario.java.restful.api.hibernate.jpa.repository.AbstractRepository;
+import com.mario.java.restful.api.hibernate.jpa.repository.PetRepository;
+import com.mario.java.restful.api.hibernate.jpa.repository.UserRepository;
 import com.mario.java.restful.api.hibernate.jpa.repository.exception.ObjectNofFoundException;
 import com.mario.java.restful.api.hibernate.jpa.service.UserService;
 
+@RequestScoped
 public class UserServiceNewImpl implements UserService {
 
-	private AbstractRepository<UserDomain, Long> abstractRepositoryUser;
-	private AbstractRepository<PetDomain, Long> abstractRepositoryPet;
+	private UserRepository userRepository;
+	private PetRepository petRepository;
 
-	public UserServiceNewImpl(AbstractRepository<UserDomain, Long> abstractRepositoryUser, AbstractRepository<PetDomain, Long> abstractRepositoryPet){
-		this.abstractRepositoryUser = abstractRepositoryUser;
-		this.abstractRepositoryPet = abstractRepositoryPet;
+	public UserServiceNewImpl(){
+	}
+
+	@Inject
+	public UserServiceNewImpl(UserRepository userRepository, PetRepository petRepository){
+		this.userRepository = userRepository;
+		this.petRepository = petRepository;
 	}
 
 	@Override
 	public void persist(UserDomain userDomain) throws Exception {
-		this.abstractRepositoryUser.persist(userDomain);
+		this.userRepository.persist(userDomain);
 	}
 
 	@Override
 	public void update(Long id, UserDomain userDomain) throws Exception, ObjectNofFoundException {
 		userDomain.setId(id);
 
-		this.abstractRepositoryUser.update(userDomain);
+		this.userRepository.update(userDomain);
 	}
 
 	@Override
@@ -39,7 +47,7 @@ public class UserServiceNewImpl implements UserService {
 
 		if(userDomain != null){
 			this.deletePets(userDomain);
-			this.abstractRepositoryUser.delete(userDomain);
+			this.userRepository.delete(userDomain);
 		} else {
 			throw new ObjectNofFoundException(id, UserDomain.class.getSimpleName());
 		}
@@ -47,38 +55,38 @@ public class UserServiceNewImpl implements UserService {
 
 	@Override
 	public void deleteAll() throws Exception, ObjectNofFoundException {
-		this.abstractRepositoryUser.deleteAll();
-		this.abstractRepositoryPet.deleteAll();
+		this.userRepository.deleteAll();
+		this.petRepository.deleteAll();
 	}
 
 	@Override
 	public UserDomain find(Long id) {
-		return this.abstractRepositoryUser.find(id);
+		return this.userRepository.find(id);
 	}
 
 	@Override
 	public List<UserDomain> findAll() {
-		return this.abstractRepositoryUser.findAll();
+		return this.userRepository.findAll();
 	}
 
 	@SuppressWarnings("hiding")
 	@Override
 	public <SingularAttribute, Object> void deleteAll(Map<SingularAttribute, Object> restrictions) throws Exception, ObjectNofFoundException {
-		this.abstractRepositoryUser.deleteAll(restrictions);
-		this.abstractRepositoryPet.deleteAll(restrictions);
+		this.userRepository.deleteAll(restrictions);
+		this.petRepository.deleteAll(restrictions);
 	}
 
 	@SuppressWarnings("hiding")
 	@Override
 	public <SingularAttribute, Object> List<UserDomain> findAll(Map<SingularAttribute, Object> restrictions) {
-		return this.abstractRepositoryUser.findAll(restrictions);
+		return this.userRepository.findAll(restrictions);
 	}
 
 	private void deletePets(UserDomain userDomain) throws Exception {
 		Map<javax.persistence.metamodel.SingularAttribute<PetDomain, UserDomain>, Object> restrictions = new HashMap<javax.persistence.metamodel.SingularAttribute<PetDomain,UserDomain>, Object>();
-		restrictions.put(PetDomain_.user, userDomain);
-		List<PetDomain> pets = this.abstractRepositoryPet.findAll(restrictions);
+		//restrictions.put(PetDomain_.user, userDomain);
+		List<PetDomain> pets = this.petRepository.findAll(restrictions);
 
-		if(pets != null) this.abstractRepositoryPet.deleteAll(pets);
+		if(pets != null) this.petRepository.deleteAll(pets);
 	}
 }
