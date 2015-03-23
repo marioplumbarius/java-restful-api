8c32ea188964597d9a235b39691fba5ada473c6e
Mario Luan <mariosouzaluan@gmail.com>
2015-03-23 02:54:22 -0300

    created repositories interfaces for User and Pet domain

---
+++ hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/repository/PetRepository.java
@@ -0,0 +1,7 @@
+package com.mario.java.restful.api.hibernate.jpa.repository;
+
+import com.mario.java.restful.api.hibernate.jpa.domain.PetDomain;
+
+public interface PetRepository extends AbstractRepository<PetDomain, Long> {
+
+}
---
+++ hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.mario.java.restful.api.hibernate.jpa.repository;
+
+import com.mario.java.restful.api.hibernate.jpa.domain.UserDomain;
+
+public interface UserRepository extends AbstractRepository<UserDomain, Long> {
+
+}
