42e07ecc410d5d4f588a0a63a700063394ae0af9
Mario Luan <mariosouzaluan@gmail.com>
2015-04-05 00:39:14 -0300

    created transient field for properties to be displayed from Domains

--- hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/domain/BaseDomain.java
+++ hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/domain/BaseDomain.java
@@ -1,6 +1,9 @@
 package com.mario.java.restful.api.hibernate.jpa.domain;
 
+import java.util.List;
+
 import javax.persistence.MappedSuperclass;
+import javax.persistence.Transient;
 
 /**
  * Abstract class which represents the base domain for all domains used by the application.<br>
@@ -11,4 +14,21 @@ import javax.persistence.MappedSuperclass;
  */
 @MappedSuperclass
 public abstract class BaseDomain extends DatedDomain {
+
+	@Transient
+	private List<String> propertiesToBeDisplayed;
+
+	/**
+	 * @return the propertiesToBeDisplayed
+	 */
+	public List<String> getPropertiesToBeDisplayed() {
+		return this.propertiesToBeDisplayed;
+	}
+
+	/**
+	 * @param propertiesToBeDisplayed the propertiesToBeDisplayed to set
+	 */
+	public void setPropertiesToBeDisplayed(List<String> propertiesToBeDisplayed) {
+		this.propertiesToBeDisplayed = propertiesToBeDisplayed;
+	}
 }
