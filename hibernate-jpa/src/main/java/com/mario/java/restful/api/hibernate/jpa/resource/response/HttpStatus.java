b339076c58cdf2cf634fe20b6f0e41ddc24b77ee
Mario Luan <mariosouzaluan@gmail.com>
2015-04-24 01:12:54 -0300

    fixed HttpStatus enum with unkown method for Family

--- hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/resource/response/HttpStatus.java
+++ hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/resource/response/HttpStatus.java
@@ -11,7 +11,7 @@ import javax.ws.rs.core.Response.StatusType;
  */
 public enum HttpStatus implements StatusType {
 
-	UNPROCESSABLE_ENTITY(422, "Unprocessed Entity");
+	UNPROCESSABLE_ENTITY(422, "Unprocessable Entity");
 
 	private final int statusCode;
     private final String reasonPhrase;
@@ -20,7 +20,7 @@ public enum HttpStatus implements StatusType {
 	HttpStatus(final int statusCode, final String reasonPhrase) {
         this.statusCode = statusCode;
         this.reasonPhrase = reasonPhrase;
-        this.family = Family.familyOf(statusCode);
+        this.family = this.getFamilyFromStatusCode(statusCode);
     }
 
 	@Override
@@ -50,5 +50,25 @@ public enum HttpStatus implements StatusType {
             }
         }
         return null;
+    }
+    
+    /**
+     * Returns its family based on its statusCode.
+     * @param statusCode the statusCode of the instance
+     * @return the family
+     */
+    private Family getFamilyFromStatusCode(int statusCode) {
+    	Family family = null;
+    	
+    	switch(statusCode/100) {
+            case 1: family = Family.INFORMATIONAL; break;
+            case 2: family = Family.SUCCESSFUL; break;
+            case 3: family = Family.REDIRECTION; break;
+            case 4: family = Family.CLIENT_ERROR; break;
+            case 5: family = Family.SERVER_ERROR; break;
+            default: family = Family.OTHER; break;
+        }
+    	
+    	return family;
     }
 }
