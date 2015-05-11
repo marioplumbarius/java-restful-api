827991adcd447616d55937ce583821a4afb0090e
Mario Luan <mariosouzaluan@gmail.com>
2015-05-10 23:28:58 -0300

    updated documentation from mapper

--- hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/mapper/PetMapper.java
+++ hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/mapper/PetMapper.java
@@ -3,6 +3,10 @@ package com.mario.java.restful.api.hibernate.jpa.mapper;
 import com.mario.java.restful.api.hibernate.jpa.dto.PetDTO;
 import com.mario.java.restful.api.hibernate.jpa.entity.PetEntity;
 
+/**
+ * Mapper for {@link PetDTO DTOs} and {@link PetEntity Entities}.
+ * @author msouz23
+ */
 public interface PetMapper extends Mapper<PetEntity, PetDTO> {
 
 }
--- hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/mapper/UserMapper.java
+++ hibernate-jpa/src/main/java/com/mario/java/restful/api/hibernate/jpa/mapper/UserMapper.java
@@ -3,6 +3,10 @@ package com.mario.java.restful.api.hibernate.jpa.mapper;
 import com.mario.java.restful.api.hibernate.jpa.dto.UserDTO;
 import com.mario.java.restful.api.hibernate.jpa.entity.UserEntity;
 
+/**
+ * Mapper for {@link UserDTO DTOs} and {@link UserEntity Entities}.
+ * @author msouz23
+ */
 public interface UserMapper extends Mapper<UserEntity, UserDTO> {
 
 }
